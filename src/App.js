cdf57a85435cb239f7f383d8b9db236343a6ad01
tmonzillo <tim.monzillo@gmail.com>
2023-09-27 12:26:36 -0400

    updates to js app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovie] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 7760854c967c720c3aa5c2c809aa0f50ce350549'
+      }
+    })
+    .then( resp => resp.json())
+    .then( resp => setMovie(resp))
+    .catch( error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie rater</h1>
       </header>
+      <div className="layout">
+          <div>
+            { movies.map( movie => {
+              return <h2>{movie.title}</h2>
+            })}
+          </div>
+          <div>
+            { movies.map( movie => {
+              return <h2>{movie.avg_rating}</h2>
+            })}
+          </div>
+      </div>
     </div>
   );
 }
